c1ecac14d0d5d8f910c445a30882f999639cd783
Anton Tuchak (nn-ultra) <anton.tuchak@gmail.com>
2017-01-24 16:08:39 +0300

    --

--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
 from distutils.core import setup
 
+from setuptools import find_packages
+
 setup(
-  name='django-csp_report',
-  packages=['csp_report'],
+  name='django-cspreport',
+  packages=find_packages(),
   version='0.1.0',
   description='Django app for handling reports from web browsers of violations of content security policy.',
   author='Anton Tuchak',
   author_email='anton.tuchak@gmail.com',
-  url='https://github.com/atuchak/django-csp_report',
-  download_url='https://github.com/atuchak/django-csp_report/tarball/0.1.0',
+  url='https://github.com/atuchak/django-cspreport',
+  download_url='https://github.com/atuchak/django-cspreport/archive/0.1.0.tar.gz',
   keywords=['Content-Security-Policy', 'csp', 'Django'],
   classifiers=[],
 )
